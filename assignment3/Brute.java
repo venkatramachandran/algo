aff787b454af3f7e0d390111db304ac1c47fdcd5
Venkatasubramanian Ramachandran <venkatasubramanian.ramachandran@ril.com>
2014-09-23 17:04:29 +0530

    Fixed a few issues in Fast

--- assignment3/Brute.java
+++ assignment3/Brute.java
@@ -4,30 +4,28 @@ public class Brute {
        In in = new In(args[0]);
        int size = in.readInt();
        Point[] points = new Point[size];
+       StdDraw.setXscale(0, 32768);
+       StdDraw.setYscale(0, 32768);
        for (int i = 0; i < size; i++) {
            int x = in.readInt();
            int y = in.readInt();
            points[i] = new Point(x, y);
+           points[i].draw();
        }
-       StdDraw.setXscale(0, 32768);
-       StdDraw.setYscale(0, 32768);
+
        StringBuffer sb = new StringBuffer();
 
        for (int p = 0; p < size; p++) {
            Point p1 = points[p];
-           //p1.draw();
            for (int q = p + 1; q < size; q++) {
                Point q1 = points[q];
                double slope1 = p1.slopeTo(q1);
-               //q1.draw();
                for (int r = q + 1; r < size; r++) {
                    Point r1 = points[r];
                    double slope2 = p1.slopeTo(r1);
-                   //r1.draw();
                    for (int s = r + 1; s < size; s++) {
                        Point s1 = points[s];
                        double slope3 = p1.slopeTo(s1);
-                       //s1.draw();
                        if (slope1 == slope2 && slope2 == slope3 && slope1 == slope3) {
                            //order the points
                            Point[] sorted = new Point[]{p1, q1, r1, s1};
--- assignment3/Fast.java
+++ assignment3/Fast.java
@@ -1,4 +1,12 @@
 public class Fast {
+   /*private static void printArray(Point o, Point[] p) {
+     System.out.println("-----");
+     System.out.println("Origin:"+o.toString());
+     for (Point p1 : p) {
+       System.out.println(p1.toString() + "/" + o.slopeTo(p1));
+     }
+     System.out.println("-----");
+   }*/
    public static void main(String[] args) {
        In in = new In(args[0]);
        int size = in.readInt();
@@ -7,26 +15,34 @@ public class Fast {
            int x = in.readInt();
            int y = in.readInt();
            points[i] = new Point(x, y);
+           points[i].draw();
        }
        StdDraw.setXscale(0, 32768);
        StdDraw.setYscale(0, 32768);
        
        int index = 0;
        for (Point p : points) {
-         p.draw();
-         if (size - index > 2) {         
-           Point[] arr = new Point[size - index];
-           System.arraycopy(points, index, arr, 0, size - index);
+         if (size - index - 1 > 2) {         
+           Point[] arr = new Point[size - index - 1];
+           System.arraycopy(points, index + 1, arr, 0, size - index - 1);
            java.util.Arrays.sort(arr, p.SLOPE_ORDER);
-           if (p.slopeTo(arr[0]) == p.slopeTo(arr[1]) && p.slopeTo(arr[0]) == p.slopeTo(arr[2])) {
+           //printArray(p, arr);
+           if (arr.length >= 4 && p.slopeTo(arr[0]) == p.slopeTo(arr[1]) && p.slopeTo(arr[0]) == p.slopeTo(arr[2]) && p.slopeTo(arr[0]) == p.slopeTo(arr[3])) {
+             //System.out.println("Found a 5 match!");
+             Point[] sorted = new Point[]{p, arr[0], arr[1], arr[2], arr[3]};
+             Insertion.sort(sorted);
+             StdOut.println(sorted[0] + " -> " + sorted[1] + " -> " + sorted[2] + " -> " + sorted[3] + " -> " + sorted[4]);
+             sorted[0].drawTo(sorted[4]);
+            }
+            else if (arr.length >= 3 && p.slopeTo(arr[0]) == p.slopeTo(arr[1]) && p.slopeTo(arr[0]) == p.slopeTo(arr[2])) {
+             //System.out.println("Found a 4 match!");
              Point[] sorted = new Point[]{p, arr[0], arr[1], arr[2]};
              Insertion.sort(sorted);
              StdOut.println(sorted[0] + " -> " + sorted[1] + " -> " + sorted[2] + " -> " + sorted[3]);
-             p.drawTo(arr[0]);
-             p.drawTo(arr[1]);
-             p.drawTo(arr[2]);
+             sorted[0].drawTo(sorted[3]);
             }
          }
+         ++index;
       }
    }
 }
