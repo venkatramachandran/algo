6150c3e24ed36ae75903dfc3cf5db779f4399031
Venkatasubramanian Ramachandran <venkatasubramanian.ramachandran@ril.com>
2014-09-23 21:11:27 +0530

    totally new logic for Fast

--- assignment3/Fast.java
+++ assignment3/Fast.java
@@ -7,6 +7,7 @@ public class Fast {
      }
      System.out.println("-----");
    }*/
+   private static final String append = " -> ";
    public static void main(String[] args) {
        In in = new In(args[0]);
        int size = in.readInt();
@@ -19,10 +20,12 @@ public class Fast {
        }
        StdDraw.setXscale(0, 32768);
        StdDraw.setYscale(0, 32768);
-       
+
+       StringBuilder sb = new StringBuilder();
+
        int index = 0;
        for (Point p : points) {
-         if (size - index - 1 > 2) {         
+         /*if (size - index - 1 > 2) {
            Point[] arr = new Point[size - index - 1];
            System.arraycopy(points, index + 1, arr, 0, size - index - 1);
            java.util.Arrays.sort(arr, p.SLOPE_ORDER);
@@ -31,16 +34,47 @@ public class Fast {
              //System.out.println("Found a 5 match!");
              Point[] sorted = new Point[]{p, arr[0], arr[1], arr[2], arr[3]};
              Insertion.sort(sorted);
-             StdOut.println(sorted[0] + " -> " + sorted[1] + " -> " + sorted[2] + " -> " + sorted[3] + " -> " + sorted[4]);
+             sb.delete(0, sb.length()).append(sorted[0]).
+                             append(append).append(sorted[1]).append(append).
+                             append(sorted[2]).append(append).append(sorted[3])
+                             .append(append).append(sorted[4]);
+             StdOut.println(sb.toString());
              sorted[0].drawTo(sorted[4]);
             }
             else if (arr.length >= 3 && p.slopeTo(arr[0]) == p.slopeTo(arr[1]) && p.slopeTo(arr[0]) == p.slopeTo(arr[2])) {
              //System.out.println("Found a 4 match!");
              Point[] sorted = new Point[]{p, arr[0], arr[1], arr[2]};
              Insertion.sort(sorted);
-             StdOut.println(sorted[0] + " -> " + sorted[1] + " -> " + sorted[2] + " -> " + sorted[3]);
+             sb.delete(0, sb.length()).append(sorted[0]).
+                             append(append).append(sorted[1]).append(append).
+                             append(sorted[2]).append(append).append(sorted[3]);
+             StdOut.println(sb.toString());
              sorted[0].drawTo(sorted[3]);
             }
+         }*/
+
+         java.util.Arrays.sort(points, index + 1, points.length, p.SLOPE_ORDER);
+         if (points.length - index > 4 && 
+           p.slopeTo(points[index + 1]) == p.slopeTo(points[index + 2]) && p.slopeTo(points[index + 1]) == p.slopeTo(points[index + 3])
+           && p.slopeTo(points[index + 1]) == p.slopeTo(points[index + 4])) {
+             Point[] sorted = new Point[]{points[index], points[index + 1], points[index + 2], points[index + 3], points[index + 4]};
+             Insertion.sort(sorted);
+             sb.delete(0, sb.length()).append(sorted[0]).
+                             append(append).append(sorted[1]).append(append).
+                             append(sorted[2]).append(append).append(sorted[3])
+                             .append(append).append(sorted[4]);
+             StdOut.println(sb.toString());
+             sorted[0].drawTo(sorted[4]);
+         }
+         else if (points.length - index > 3 && 
+           p.slopeTo(points[index + 1]) == p.slopeTo(points[index + 2]) && p.slopeTo(points[index + 1]) == p.slopeTo(points[index + 3])) {
+             Point[] sorted = new Point[]{points[index], points[index + 1], points[index + 2], points[index + 3]};
+             Insertion.sort(sorted);
+             sb.delete(0, sb.length()).append(sorted[0]).
+                             append(append).append(sorted[1]).append(append).
+                             append(sorted[2]).append(append).append(sorted[3]);
+             StdOut.println(sb.toString());
+             sorted[0].drawTo(sorted[3]);
          }
          ++index;
       }
